3782af27798c5dac3507f7d9d083b333821c536f
unknown <livv89>
2025-08-22 15:38:39 +0900

    1

---
+++ test1.cpp
@@ -0,0 +1,26 @@
+//	Hello 'C' World
+ #include <stdio.h>
+ //#include <stdafx.h>
+ int main(void)
+ {
+ 	int age, score; // 정수 변수 선언문
+	  
+	  printf("나이가 어떻게 되세요? : ");
+	  scanf("%d", &age);
+	  printf("점수는요? : ");
+	  scanf("%d", &score);
+
+	  printf("\n\nMy age : %d \n", age);
+	  printf("%d is my point \n", score);
+ 	
+/*	printf("My age : %d  \n", 25, 100);
+	printf("%d is my point \n", 100);
+	printf("Good \nmorning \neverybody\n");
+	return 0; */
+	
+//	printf("My age : %d \n%d is my point \nGood \nmorning \neverybody\n", 25, 100);
+	
+	
+	
+ }
+  
