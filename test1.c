2bcdcaaeb8c55fbbc169501b34abae110ecbb429
unknown <livv89>
2025-08-22 15:55:29 +0900

    1

--- test1.c
+++ test1.c
@@ -13,13 +13,7 @@
 	  printf("\n\nMy age : %d \n", age);
 	  printf("%d is my point \n", score);
  	
-/*	printf("My age : %d  \n", 25, 100);
-	printf("%d is my point \n", 100);
-	printf("Good \nmorning \neverybody\n");
-	return 0; */
-	
-//	printf("My age : %d \n%d is my point \nGood \nmorning \neverybody\n", 25, 100);
-	
+
 	
 	
  }
