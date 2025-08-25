a6ac4745698582f1c2621707e962fd367e17c3f5
unknown <livv89>
2025-08-25 16:53:12 +0900

    1

---
+++ C/test5.C
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+	//시작값, 종료값 입력
+	int sval, eval, rval;
+	printf("시작값을 입력하세요 "); scanf("%d", &sval);
+	printf("종료값을 입력하세요 "); scanf("%d", &eval);
+	
+	rval = 0;
+	int index = sval;
+	
+	while((index >= sval)&&(index <= eval))  //수행 조건식 
+	{
+		
+	}
+	rval = rval + sval; sval++;
+}
